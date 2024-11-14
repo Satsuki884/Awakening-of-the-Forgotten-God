0b6c68a2242cb804323a2a7c908d47a13160b714
Satsuki884 <62888563+Satsuki884@users.noreply.github.com>
2024-11-14 12:50:53 +0300

    update settings

--- Assets/Scripts/Setting/SettingController.cs
+++ Assets/Scripts/Setting/SettingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingController : MonoBehaviour
 {
@@ -12,15 +13,35 @@ public class SettingController : MonoBehaviour
     [SerializeField] private Slider _volumeSlider;
 
     [SerializeField] private AudioMixer _audioMixer;
-    
+
     public void SetVolume()
     {
         Debug.Log(_volumeSlider.value);
         _audioMixer.SetFloat("Volume", _volumeSlider.value);
     }
 
+    Resolution[] resolutions;
+
     private void Start()
     {
+        resolutions = Screen.resolutions;
+        _resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width &&
+            resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        _resolutionDropdown.AddOptions(options);
+        _resolutionDropdown.value = currentResolutionIndex;
+        _resolutionDropdown.RefreshShownValue();
+        
         AddEventListeners();
         CloseSettings();
     }
@@ -33,6 +54,9 @@ public class SettingController : MonoBehaviour
         _openSettingsButton.onClick.AddListener(OpenSettings);
         _closeSettingsButton.onClick.AddListener(CloseSettings);
         _volumeSlider.onValueChanged.AddListener(delegate { SetVolume(); });
+        _qualityDropdown.onValueChanged.AddListener(delegate { SetQuality(); });
+        _fullScreenToggle.onValueChanged.AddListener(delegate { SetFullScreen(); });
+        _resolutionDropdown.onValueChanged.AddListener(delegate { SetResolution(); });
     }
 
     private void OpenSettings()
@@ -43,5 +67,29 @@ public class SettingController : MonoBehaviour
     private void CloseSettings()
     {
         _settingPanel.SetActive(false);
+    }
+
+    [SerializeField] private TMP_Dropdown _qualityDropdown;
+
+    public void SetQuality()
+    {
+        Debug.Log(_qualityDropdown.value);
+        QualitySettings.SetQualityLevel(_qualityDropdown.value);
+    }
+
+    [SerializeField] private Toggle _fullScreenToggle;
+
+    public void SetFullScreen()
+    {
+        Screen.fullScreen = _fullScreenToggle.isOn;
+    }
+
+    [SerializeField] private TMP_Dropdown _resolutionDropdown;
+
+    public void SetResolution()
+    {
+        Debug.Log(_resolutionDropdown.value);
+        Resolution resolution = resolutions[_resolutionDropdown.value];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
