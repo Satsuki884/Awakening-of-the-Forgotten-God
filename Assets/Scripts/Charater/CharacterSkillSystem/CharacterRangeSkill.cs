a9f03144955f6530d577e43ea1e896be6e50c589
Satsuki884 <62888563+Satsuki884@users.noreply.github.com>
2024-12-08 22:14:53 +0200

    fixed idzanami range skill

--- Assets/Scripts/Charater/CharacterSkillSystem/CharacterRangeSkill.cs
+++ Assets/Scripts/Charater/CharacterSkillSystem/CharacterRangeSkill.cs
@@ -45,50 +45,49 @@ namespace AFG.Character
             OnTargetSelected(AITarget);
         }
 
+        Vector3 targetPosition;
+
+
         protected override void OnTargetSelected(CharacterController characterController)
         {
             base.OnTargetSelected(characterController);
+            targetPosition = characterController.transform.position;
 
-            Vector3 targetPosition = characterController.transform.position;
+            //start hit enemy
+            _user.AnimationController.PlayRangeAttackAnimation(_user, () =>
+            {
+                //ParticleMove(() =>
+                //{
+                    characterController.DamageController.TakeDamage(_user.Atk, characterController);
+                    _user.AnimationController.PlayIdleAnimation(_user);
+                    onSkillUsed?.Invoke();
+                //});
+            });
+        }
+
+        public void ParticleMove(/*Action OnMoveFinished*/)
+        {
             Vector3 direction = (targetPosition - _user.transform.position).normalized;
             Vector3 adjustedPosition = targetPosition - direction * 3f;
             adjustedPosition.y += 1;
             targetPosition.y += 1;
-            //start hit enemy
-            _user.AnimationController.PlayRangeAttackAnimation(_user, () =>
-            {
-                MoveTo(adjustedPosition, () =>
-                    {
-                        _hitVfx.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-
-                        if (_vfx == null)
-                        {
-                            _vfx = Instantiate(_rangeVfxPrefab, targetPosition, Quaternion.identity).GetComponent<ParticleSystem>();
-                        }
-                        _vfx.Play();
-                        //enemy hit
-                        characterController.DamageController.TakeDamage(_user.Atk, characterController);
-                        // _vfx.Stop(true, ParticleSystemStopBehavior.StopEmitting);
-                        //play idle animation on start point
-                        _user.AnimationController.PlayIdleAnimation(_user);
-                        onSkillUsed?.Invoke();
-                    });
-
-            });
-        }
-
-        public void MoveTo(Vector3 transformPosition, Action OnMoveFinished)
-        {
-            if (_hitVfx == null)
-            {
+            // if (_hitVfx == null)
+            // {
                 _hitVfx = Instantiate(_hitVfxPrefab, _user.transform.position, Quaternion.identity).GetComponent<ParticleSystem>();
-            }
+            // }
             _hitVfx.Play();
 
-            _hitVfx.transform.DOMove(transformPosition, 1f).SetEase(Ease.Linear).OnComplete(() =>
+            _hitVfx.transform.DOMove(adjustedPosition, 1f).SetEase(Ease.Linear).OnComplete(() =>
             {
                 _hitVfx.transform.DOKill();
-                OnMoveFinished?.Invoke();
+                _hitVfx.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+
+                if (_vfx == null)
+                {
+                    _vfx = Instantiate(_rangeVfxPrefab, targetPosition, Quaternion.identity).GetComponent<ParticleSystem>();
+                }
+                _vfx.Play();
+                //OnMoveFinished?.Invoke();
             });
         }
     }
